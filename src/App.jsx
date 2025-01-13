7a93b52e0291d83330c4cc4820ee4f8e31082167
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2025-01-13 18:05:46 +0800

    ggss

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <div className="w-screen h-screen flex justify-center items-center bg-gray-400">
+        <div className="w-full h-full rounded-3xl flex bg-neatral-100">
+          <div className="w-1/2 h-full bg-neutral-100 flex justify-center items-center ">
+            <div className="w-1/2 h-3/4 bg-red-500 rounded-3xl"></div>
+          </div>
+          <div className="w-1/2 h-full bg-slate-900 flex justify-center items-center ">
+            <div className="w-1/2 h-3/4 bg-green-500 rounded-3xl"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
